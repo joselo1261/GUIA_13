e913a79a85c205cc0a61dffcadd25d98aebdecd0
Jose Luis Elisseche <joselise61@gmail.com>
2023-06-28 15:38:52 -0300

    Actualizar Excepciones

---
+++ Excepciones.java
@@ -0,0 +1,23 @@
+
+public class Excepciones{
+
+    public static void main(String[] args) {
+    try {
+     int resultado = 3/0;
+    
+    } catch (Exception e) {
+        System.err.println("No se puede dividir por cero");   
+    }
+        
+
+    int edades[]= {10,20,30,40};
+    try {
+       System.out.println("La edad en posicion 4 es:"+ edades[4]);   
+    } 
+    catch (Exception e) {
+        System.out.println("Fuera de Rango");    
+    }
+}
+
+
+}
